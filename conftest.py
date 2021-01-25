d0ab06beb5bf24336b4a57149f5160c82f8a39ad
wkudashov <77318452+wkudashov@users.noreply.github.com>
2021-01-25 18:00:52 +0400

    Update conftest.py
    added --browser_name option. chrome or firefox. chrome by default.

--- conftest.py
+++ conftest.py
@@ -1,24 +1,36 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="chose the page language for testing")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="choose testing browser: chrome or firefox")
 
 
 @pytest.fixture()
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    options = Options()
-
-    # options.add_argument("--headless")
-    # запуск браузера в режиме работы без отрисовки интерфейса
-
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    
+    if browser_name == "chrome":
+        c_options = webdriver.chrome.options.Options()
+        # c_options.add_argument("--headless")
+        c_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        c_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+        
+    elif browser_name == "firefox":
+        f_options = webdriver.firefox.options.Options()
+        # f_options.headless = True
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=f_options, firefox_profile=profile)
+    
+    else:
+        raise pytest.UsageError("--browser_name should be 'chrome' or 'firefox'")
+    
     yield browser
+    
     browser.quit()
-
