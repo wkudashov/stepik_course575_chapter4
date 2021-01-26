f1ea91c9183c7c65f2083e8b39ca18d09a690875
wkudashov <wkudashov@gmail.com>
2021-01-26 17:46:00 +0400

    login_page.py object added checking for login link, reg form and login form

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.ID, "login_link")
+
+
+class LoginPageLocators:
+    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTRATION_FORM = (By.ID, "register_form")
---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url and self.is_element_present(*LoginPageLocators.LOGIN_LINK), \
+               "Login link is not presented on Login"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+               "Login form is not presented on Login"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), \
+               "Registration form is not presented on Login"
