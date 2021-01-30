fe0cd9893d30fd49b5608759f0185169bb9e1303
wkudashov <77318452+wkudashov@users.noreply.github.com>
2021-01-30 14:04:22 +0400

    Update test_product_page.py
    parametrizing returned in test_guest_can_add_product_to_basket.

--- test_product_page.py
+++ test_product_page.py
@@ -38,8 +38,9 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
+@pytest.mark.parametrize('offer_num', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, offer_num):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{str(offer_num)}'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
